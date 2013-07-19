ccea72ca93df279836ba746e37b91710b60a08b0
johnny arias <jarias6@yahoo.com>
2013-07-18 22:51:42 -0400

    Updated web.js to read from disk

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
+
 var app = express();
 app.use(express.logger());
+ 
+var text =  fs.readFileSync('./index.html','utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+	response.send(text);
+    });
 
-var port = process.env.PORT || 5000;
+
+var port  = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 }); 
