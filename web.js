0f2d7b0aa749cd3a0bcec6b63cd438e475074210
johnny arias <jarias6@yahoo.com>
2013-07-21 13:19:57 -0400

    Trying to log to console

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+// Include http module,
+var http = require("http"),
+// And mysql module you've just installed.
+fs = require("fs");
 
 var app = express();
-app.use(express.logger())
+app.use(express.logger());
 
-fs.readFile('index.html',function (error, data));
+fs.readFile('index.html',function (error, data) {
+if (error) 
+{
+throw error;
+}
+console.log(data);
+});
 
 app.get('/', function(request, response) {
 	response.send(data);
