7f6be70784731ced26f0d3e94436d5faa98aa752
johnny arias <jarias6@yahoo.com>
2013-07-21 13:05:44 -0400

    try again

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express');
 
 var app = express();
-app.use(express.logger());
- 
-var text =  fs.readFileSync('./index.html','utf8');
+app.use(express.logger())
+
+fs.readFile('index.html',function (error, data));
 
 app.get('/', function(request, response) {
-	response.send(text);
+	response.send(data);
     });
-
 
 var port  = process.env.PORT || 5000;
 app.listen(port, function() {
