228f0e59c5e2c7bcc9ddb3d635e04aeb5d872581
johnny arias <jarias6@yahoo.com>
2013-07-21 16:06:36 -0400

    changed

--- web.js
+++ web.js
@@ -8,16 +8,17 @@ fs = require("fs");
 var app = express();
 app.use(express.logger());
 
-var content;
-fs.readFile('/Users/jonellarias/bitstarter/index.html', function (err, data) {
-if (err) {
-            throw err;
-         }
-         });
+//var content;
+//fs.readFile('index.html', function (err, data) {
+//if (err) {
+//            throw err;
+//         }
+//         });
 
 app.get('/', function(request, response) {
+    fs.readFile('index.html',function (err, data){
 	response.send(data);
-	
+	}); 
     });
 
 var port  = process.env.PORT || 5000;
