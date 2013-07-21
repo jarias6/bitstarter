62fb1d4938da3bb8baec17a733431d60422cd643
johnny arias <jarias6@yahoo.com>
2013-07-21 18:02:37 -0400

    hjhdgd

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
-//var express = require('express');
+var express = require('express');
 //var sys = require('sys');
 
-//fs = require("fs");
+fs = require("fs");
 
-//var app = express();
+var app = express();
 //app.set("view options", {layout: false});
-//app.use(express.static(__dirname + '/bitstarter'));
+app.use(express.static(__dirname + '/bitstarter'));
 //app.use(express.static(__dirname + '/Users'));
 
-//app.use(express.logger());
+app.use(express.logger());
 
-//var index = fs.readFileSync("index.html"   );
-//console.log(index);
+var index = fs.readFileSync('index.html','utf-8' );
+console.log(index);
 
 //var content;
 //fs.readFile('index.html', function (err, data) {
@@ -28,34 +28,34 @@
 //	}); 
 //    });
 
-//var port  = process.env.PORT || 8080;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//}); 
+var port  = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+}); 
 
 
 // Include http module,
-var http = require("http"),
+//var http = require("http"),
 // And mysql module you've just installed.
-   fs = require("fs");
+  // fs = require("fs");
  
 // Create the http server.
-http.createServer(function (request, response) {
+//http.createServer(function (request, response) {
    // Attach listener on end event.
-   request.on("end", function () {
+  // request.on("end", function () {
       // Read the file.
-      fs.readFile('./index.html', 'utf-8', function (error, data) {
+    //  fs.readFile('./index.html', 'utf-8', function (error, data) {
          // Write headers.
-         response.writeHead(200, {
-            'Content-Type': 'text/plain'
-         });
+      //   response.writeHead(200, {
+        //    'Content-Type': 'text/plain'
+       //  });
          // Increment the number obtained from file.
          //data = parseInt(data) + 1;
          // Write incremented number to file.
          //fs.writeFile('test.txt', data);
          // End response with some nice message.
-         response.end('This page was refreshed ' + data + ' times!');
-      });
-   });
+         //response.end('This page was refreshed ' + data + ' times!');
+      //});
+   //});
 // Listen on the 8080 port.
-}).listen(8080);
+//}).listen(8080);
